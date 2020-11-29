9162ad341f6ffb367ff6195d5f09ebf10dc4e27a
May27Oliver <oliver310338@hotmail.com>
2020-11-29 12:53:02 +0800

    Updates

---
+++ precache-manifest.670084fe2f79f87b1008027cd2724f65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6848aaf83f17380ee75c663a4d153d12",
+    "url": "/resume_page/index.html"
+  },
+  {
+    "revision": "f76b7dc5b55bc3900c8a",
+    "url": "/resume_page/static/css/main.d5683e46.chunk.css"
+  },
+  {
+    "revision": "17681eb122310bf725ae",
+    "url": "/resume_page/static/js/2.13ff0e09.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/resume_page/static/js/2.13ff0e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76b7dc5b55bc3900c8a",
+    "url": "/resume_page/static/js/main.1adfcca2.chunk.js"
+  },
+  {
+    "revision": "7957fe74122225020874",
+    "url": "/resume_page/static/js/runtime-main.928bfb3a.js"
+  },
+  {
+    "revision": "65e1b62a00f08a7869bfce5c9c8e96ec",
+    "url": "/resume_page/static/media/coast.65e1b62a.jpg"
+  },
+  {
+    "revision": "f5f10a322899acbe178c25fdd374acd6",
+    "url": "/resume_page/static/media/protrait.f5f10a32.jpg"
+  }
+]);
