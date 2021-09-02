6f4913d71397e699c188dc8dd19b646816dca3aa
oliver310338 <oliver310338@hotmail.com>
2021-09-02 21:01:32 +0800

    履歷更新

--- src/resume/App.js
+++ src/resume/App.js
@@ -1,75 +1,78 @@
-import React, { Component,Fragment,createRef } from 'react';
-import resume from "./resum.json" 
-import TimeLine from './TimeLine'
-import './style/style.css'
+import React, { Component, Fragment, createRef } from "react";
+import resume from "./resum.json";
+import TimeLine from "./TimeLine";
+import "./style/style.css";
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.myAddress=createRef();
-    }
-    copiedEmail=(e)=>{
-        let address = document.getElementById('myEmailAddress');
-        address.select();
-        document.execCommand("Copy");
-    }
-    render() {
-        return (
-            <Fragment>
-                <div className="protrait">
-                    <h1 className="about-me">關於我</h1>
-                    <div className="self-protrait-zone">
-                        <div className="self-portrait-box">
-                            <div className="self-protrait"></div>
-                        </div>
-                        <div className="id-card">
-                            <div className="card">
-                                <div className="personal-name">
-                                    <h2 className="tw-name">
-                                        陳韋齊<label className="en-name">  Oliver Chen</label>
-                                    </h2>
-                                    
-                                </div>
-                                <div className="sidetation">
-                                    擁有一台相機的前端工程師。
-                                </div> 
-                                <div className="intro-prief">
-                                    <span className="En">
-                                       1+ Years of Front-end Developer<br/>
-                                        Focus on :<br/>
-                                        JavaScript (Framework such as AngularJS、React、Vue )<br/>
-                                        CSS ( SASS、Styled-Component )<br/>
-                                        HTML ( JSX )<br/>
-                                        Git<br/>
-                                        <br/>
-                                        E-mail: (click it will copy!) <input id='myEmailAddress' onClick={this.copiedEmail} value="oliver310338@hotmail.com" readonly="true"></input>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  constructor(props) {
+    super(props);
+    this.myAddress = createRef();
+  }
+  copiedEmail = (e) => {
+    let address = document.getElementById("myEmailAddress");
+    address.select();
+    document.execCommand("Copy");
+  };
+  render() {
+    return (
+      <Fragment>
+        <div className="protrait">
+          <h1 className="about-me">關於我</h1>
+          <div className="self-protrait-zone">
+            <div className="self-portrait-box">
+              <div className="self-protrait"></div>
+            </div>
+            <div className="id-card">
+              <div className="card">
+                <div className="personal-name">
+                  <h2 className="tw-name">
+                    陳韋齊<label className="en-name"> Oliver Chen</label>
+                  </h2>
                 </div>
-                <div className="ex-resume">
-                    <div className="experience wrap">
-                        <div className="column-title">
-                            <h3 className="title">經歷</h3>
-                        </div>
-                        <TimeLine resume={resume.work_exp}></TimeLine>
-                    </div>
+                <div className="sidetation">擁有一台相機的前端工程師。</div>
+                <div className="intro-prief">
+                  <span className="En">
+                    2+ Years of Front-end Developer
+                    <br />
+                    Focus on :<br />
+                    JavaScript (Framework such as AngularJS、React、Vue )<br />
+                    CSS ( SASS、Styled-Component )<br />
+                    HTML ( JSX )<br />
+                    Git
+                    <br />
+                    <br />
+                    E-mail: (click it will copy!){" "}
+                    <input
+                      id="myEmailAddress"
+                      onClick={this.copiedEmail}
+                      value="oliver310338@hotmail.com"
+                      readonly="true"
+                    ></input>
+                  </span>
                 </div>
-                <div className="resume">
-                    <div className="education wrap">
-                        <div className="column-title">
-                            <h3 className="title">學歷</h3>
-                        </div>
-                        <TimeLine resume={resume.resume}></TimeLine>
-                    </div>
-                </div>
-                
-            </Fragment>
-            
-        );
-    }
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="ex-resume">
+          <div className="experience wrap">
+            <div className="column-title">
+              <h3 className="title">經歷</h3>
+            </div>
+            <TimeLine resume={resume.work_exp}></TimeLine>
+          </div>
+        </div>
+        <div className="resume">
+          <div className="education wrap">
+            <div className="column-title">
+              <h3 className="title">學歷</h3>
+            </div>
+            <TimeLine resume={resume.resume}></TimeLine>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
